fc9a4f27019c95e59dd4ace8aea8e6854de0dc40
Sid <isrodick@gmail.com>
2013-08-19 15:40:20 +0300

    filled models.py and generated the operation CREAT TABLE

--- mysite/settings.py
+++ mysite/settings.py
@@ -21,7 +21,7 @@ DATABASES = {
         #'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         #'PORT': '',                      # Set to empty string for default.
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'books.models.py'
+        'NAME': 'books.db'
     }
 }
 #бабуйня!
